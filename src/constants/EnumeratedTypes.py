68ef6139f471ccef7d4d781ebfb69743b131b564
Jacob Shiohira <jlshiohira23@gmail.com>
2019-01-13 12:42:15 -0600

    Add enums and constants needed for japanese verb conjugation app

---
+++ src/constants/EnumeratedTypes.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+class Formality(Enum):
+    PLAIN = 1
+    POLITE = 2
+
+class Polarity(Enum):
+    POSITIVE = 1
+    NEGATIVE = 2
+
+class Tense(Enum):
+    PAST = 1
+    NONPAST = 2
+
+class VerbClass(Enum):
+    GODAN = 1
+    ICHIDAN = 2
+    IRREGULAR = 3
+    NONIRREGULAR = 4
