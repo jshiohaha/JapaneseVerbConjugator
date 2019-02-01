802f44076d3109a8d30dc8a84cb00efe70b0fd5d
Jacob Shiohira <jlshiohira23@gmail.com>
2019-01-31 22:41:23 -0600

    Update test shell script to take use coverage package

--- RunTests.sh
+++ RunTests.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-filename="JapaneseVerbFormGeneratorTests.py"
-python "tests/$filename"
+japaneseVerbFormGeneratorTests="JapaneseVerbFormGeneratorTests.py"
+utilsTests="UtilsTests.py"
+if [ $# -gt 0 ]
+  then
+    srcdir = "src/"
+    coverage run -a --source $srcdir "tests/$japaneseVerbFormGeneratorTests"
+    coverage run -a --include "src/Utils.py" "tests/$utilsTests"
+    if [ $1 == "report" ]
+    then
+      coverage report -m # prints to console
+    elif [ $1 == "html" ]
+    then
+      coverage html # this will create a dir named htmlcov/ which contains html coverage data
+      open htmlcov/index.html
+    fi
+else
+  python "tests/$japaneseVerbFormGeneratorTests"  
+  python "tests/$utilsTests"
+fi
